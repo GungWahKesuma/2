a57b411c697d1c6ab40eae52aec7fe37d05fff19
GungWahKesuma <32163815+GungWahKesuma@users.noreply.github.com>
2020-03-07 21:28:15 +0800

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,39 @@
+cashback = [500, 1000, 2000, 5000, 10000, 20000, 50000]
+
+def menu():
+    print("\n")
+    print("-------menu-------")
+    print("[1]. Input money")
+    print("[2]. exit")
+    print("------------------")
+
+    Menu = input("pilih : ")
+
+    if int(Menu) == 1:
+        cash()
+    elif int(Menu) == 2:
+        exit()
+
+    else:
+        print("\n")
+        print ("Wrong Choise!")
+        print("\n")
+        menu()
+
+def cash():
+    Cash = input("Money : ")
+    print("\n")
+    total = int(Cash)
+    qy = 0
+    csh = 6
+    for i in range(7):
+        while(total>=cashback[csh]):
+            total = total-cashback[csh]
+            qy = qy + 1
+        if qy > 0:
+            print("> %d x %d" % (qy, cashback[csh]))
+        csh = csh - 1
+        qy = 0
+    print("\n")
+    menu()
+menu()
