e923e6d980895b8cb5619c0d3b864dc88177e289
GungWahKesuma <32163815+GungWahKesuma@users.noreply.github.com>
2020-03-08 13:59:38 +0800

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
-cashback = [500, 1000, 2000, 5000, 10000, 20000, 50000]
+cashback = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
 
 def menu():
     print("\n")
@@ -24,16 +24,17 @@ def cash():
     Cash = input("Money : ")
     print("\n")
     total = int(Cash)
+    cashS = len(cashback)-1
+    csh = cashS
     qy = 0
-    csh = 6
-    for i in range(7):
+    i = 0
+    for i in range(i,cashS):
         while(total>=cashback[csh]):
             total = total-cashback[csh]
             qy = qy + 1
         if qy > 0:
-            print("> %d x %d" % (qy, cashback[csh]))
+            print("> %d x Rp.%d" % (qy, cashback[csh]))
         csh = csh - 1
         qy = 0
-    print("\n")
     menu()
 menu()
